7442a462f7ffaaaa3548a1eade334a10c2e7b550
naoki1022 <kuro_legatta_b4@yahoo.co.jp>
2021-11-26 04:59:41 +0900

    商品購入機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     redirect_to root_path if @item.user_id != current_user.id
+    if @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def update
